8b91a5b481dad437ad93a769c610e1599b21daae
rafmasloman <raflymasloman20@gmail.com>
2021-10-07 12:05:01 +0800

    manambah reponsive mobile 375px

--- src/components/header/navbar.js
+++ src/components/header/navbar.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-function navbar() {
+import Hamburger from "../../images/icons/icon-hamburger.svg";
+function Navbar() {
+  const [isHidden, setHidden] = useState(false);
   return (
     <nav className="container mx-auto flex md:flex-row flex-col justify-between md:px-40 py-8">
-      <div className="">
+      <div className="flex justify-between">
         <span className="text-blue-secondary font-primary font-bold text-2xl">
           NPE<span className="text-blue-primary">Digital</span>{" "}
         </span>
+        <div className="flex md:hidden">
+          <button className="" onClick={() => setHidden(!isHidden)}>
+            <img src={Hamburger} alt="HamburgerIcon" />
+          </button>
+          {/* <button onClick={() => console.log(isHidden)}>test</button> */}
+        </div>
       </div>
-      <div className="flex md:flex-row flex-col font-primary md:items-center mt-5 md:mt-0">
+      <div
+        className={`${
+          isHidden ? "flex" : "hidden"
+        } md:flex md:flex-row flex-col font-primary md:items-center mt-5 md:mt-0`}
+      >
         <Link className="text-blue-secondary md:px-10 py-4 md:py-0" to="#">
           Home
         </Link>
@@ -21,7 +33,7 @@ function navbar() {
           About
         </Link>
       </div>
-      <div className="flex items-center">
+      <div className={` ${isHidden ? "flex" : "hidden"}  md:flex items-center`}>
         <Link className="font-primary text-blue-secondary py-4 md:py-0" to="#">
           Contact Us
         </Link>
@@ -30,4 +42,4 @@ function navbar() {
   );
 }
 
-export default navbar;
+export default Navbar;
