76361680be72a61b8e64a21f7a6192d4e288ae65
rafmasloman <raflymasloman20@gmail.com>
2021-10-08 09:35:40 +0800

    push

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Header from "./components/header";
-
+import Hero from "./components/hero";
 function App() {
-  return <Header />;
+  return (
+    <div className="px-7.5 md:px-40">
+      <Header />
+      <Hero />
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,32 @@ module.exports = {
           primary: "#006CE2",
           secondary: "#113466",
         },
+        // * Warna gray untuk digunakan pada text secondary
+        gray: {
+          text: "#B7B7B7",
+        },
       },
+      //* Font dari google font, font primary untuk text berupa headline dan secondary untuk text secondary
       fontFamily: {
         primary: ["Quicksand", "sans-serif"],
         secondary: ["Nunito", "sans-serif"],
+      },
+      fontSize: {
+        "2.5xl": "1.75rem",
+        "6.5xl": "4rem",
+      },
+      padding: {
+        7.5: "1.875rem",
+      },
+      margin: {
+        12.5: "3.125rem",
+        7.5: "1.875rem",
+      },
+      borderRadius: {
+        "2.5xl": "1.25rem",
+      },
+      boxShadow: {
+        "primary-btn": "0 5px 20px rgba(2, 108, 255, 0.3)",
       },
     },
   },
